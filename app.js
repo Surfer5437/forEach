79cc6320cea7b84604f4a2c8f742b14d5c67c93b
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-11 17:47:44 -0500

    added valTimes Index function

--- app.js
+++ app.js
@@ -125,3 +125,16 @@ function doubleValuesWithMap(values) {
 }
 
 
+// valTimesIndex
+// Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
+
+// valTimesIndex([1,2,3]) // [0,2,6]
+// valTimesIndex([1,-2,-3]) // [0,-2,-6]
+
+
+function valTimesIndex(values) {
+    const newArr = values.map(num => {
+        return num * values.indexOf(num);
+    });
+    return newArr;
+}
