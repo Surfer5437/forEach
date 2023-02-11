7cfbce393cf20a6d9e967b4684ef31bc76f4004e
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-11 18:50:16 -0500

    added find function

--- app.js
+++ app.js
@@ -212,3 +212,15 @@ function filterByValue(arr, keyName) {
     });
     return val;
 };
+
+
+// find
+// Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+
+// find([1,2,3,4,5], 3) // 3
+// find([1,2,3,4,5], 10) // undefined
+
+function find(arr, keyName) {
+    const i = arr.indexOf(keyName);
+    return arr[i];
+};
