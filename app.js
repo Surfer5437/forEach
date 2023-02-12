9882638f90240c0e7a2359eb1ad7ac7a659d36c9
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-11 20:22:08 -0500

    added doubleOddNumbers and finished project

--- app.js
+++ app.js
@@ -269,3 +269,21 @@ function removeVowels(str) {
     };
     return final;
 };
+
+// doubleOddNumbers
+// Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+// doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
+// doubleOddNumbers([4,4,4,4,4]) // []
+
+
+function doubleOddNumbers(arr) {
+    const newArr = []
+    arr.forEach(element => {
+        if (element % 2 === 1) {
+            newArr.push(element * 2);
+        };
+    });
+
+    return newArr;
+}
